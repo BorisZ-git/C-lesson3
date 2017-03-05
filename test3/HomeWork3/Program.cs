a29b22ecda139d715212c15fedc6f5614a03885c
BorisZ-git <ribos@bk.ru>
2017-03-05 22:11:30 +0300

    copy past NOD method

--- test3/HomeWork3/Program.cs
+++ test3/HomeWork3/Program.cs
@@ -16,11 +16,11 @@ namespace HomeWork3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(3 % 5);
-            Console.WriteLine(3 / 4);
+            Console.WriteLine(5 % 3);
+            Console.WriteLine(9 / 2);
 
-            Fractions frs1 = new Fractions(2, 5);
-            Fractions frs2 = new Fractions(3, 4);
+            Fractions frs1 = new Fractions(120, 426);
+            Fractions frs2 = new Fractions(6, 426);
             Fractions frs3 = new Fractions();
             frs3 = frs1.Plus(frs2);
             frs3.ShowRes();
@@ -29,10 +29,12 @@ namespace HomeWork3
         }
         class Fractions
         {
+            // целое число
+            int integer;
             // числитель
-            int numerator;
+            int num;
             // знаменатель 
-            int denominator;
+            int den;
             //constructor empty
             public Fractions()
             {
@@ -41,37 +43,56 @@ namespace HomeWork3
             //constructor take value
             public Fractions(int numerator, int denominator)
             {
-                this.denominator = denominator;
-                this.numerator = numerator;
+                den = denominator;
+                num = numerator;
             }
             //Realize method Plus
             public Fractions Plus(Fractions x)
             {
                 Fractions y = new Fractions();
-                if (denominator==x.denominator)
+                //int ComMul = 0;
+
+                if (den==x.den)
                 {
-                    y.denominator = denominator;
-                    y.numerator = numerator + x.numerator;
+                    y.den = den;
+                    y.num = num + x.num;
+                    //simplifying fractions
+                    for (integer = 0; y.num > y.den; ++integer) y.num = y.num - y.den;
+
                 }
                 //We need to set all fractions to one denominator
                 else
                 {
                     //find equal denominator
-                    int EqualDenominator = 0;
-                    for (int i = 1; i % denominator != 0 
-                        | i % x.denominator != 0; EqualDenominator = ++i) ;                    
+                    int EqualDen = 0;
+                    for (int i = 1; i % den != 0 | i % x.den != 0; EqualDen = ++i);                    
                     //reduce to a common denominator
-                    y.numerator = (numerator * (EqualDenominator / denominator)) +
-                        (x.numerator * (EqualDenominator / x.denominator));
-                    y.denominator = EqualDenominator;
+                    y.num = (num * (EqualDen / den)) + (x.num * (EqualDen / x.den));
+                    y.den = EqualDen;
+                    //simplifying fractions
+                    for (integer = 0; y.num > y.den; ++integer) y.num = y.num - y.den;
+
+                    //for (int i = 1; y.den % i == 0 | y.num % i == 0; ComMul = ++i) ;
+                    //y.den = y.den / ComMul;
+                    //y.num = y.num / ComMul;
+
                 }
                 return y;
             }
             //Realize method ShowRes
             public void ShowRes()
             {
-                Console.WriteLine($"{numerator}/{denominator}");
+                Console.WriteLine($"{integer} {num}/{den}");
             }
+            //Get NOD
+            static int NOD(int a,int b)
+            {
+                while (a != b)
+                {
+                    if (a > b) a = a - b; else b = b - a;
+                }
+                return a;
+            }            
 
         }
     }
