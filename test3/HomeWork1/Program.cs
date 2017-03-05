b03215d791fc3c31b5bc68a3f1499e26c6a186c0
BorisZ-git <ribos@bk.ru>
2017-03-05 13:04:55 +0300

    test method from struct Complex

--- test3/HomeWork1/Program.cs
+++ test3/HomeWork1/Program.cs
@@ -8,14 +8,60 @@ using System.Threading.Tasks;
  * Продемонстрировать работу структуры;
  * 1.2 Дописать класс Complex, добавив методы вычитания и произведения чисел.
  * Проверить работу класса;
- * */ 
+ * */
+#region Комплексное число
+//Комплексное число — это выражение вида a + bi, где a, b — действительные числа,
+//а i — так называемая мнимая единица, символ, квадрат которого равен –1, то есть i2 = –1.
+//Число a называется действительной частью, а число b — мнимой частью комплексного числа z = a + bi.
+//Если b = 0, то вместо a + 0i пишут просто a.
+//Видно, что действительные числа — это частный случай комплексных чисел.
+#endregion
 
 namespace HomeWork1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Complex manipulation = new Complex();
+            manipulation.im = 7;
+            manipulation.re = 10;
+            manipulation.Plus(manipulation);
+            Console.ReadLine();
+
+            
+        }
+        struct Complex
+        {
+            public double im;
+            public double re;
+            // в C# в структурах могут храниться так же действия над данными
+            public Complex Plus(Complex x)
+            {
+                Complex y;
+                y.im = im + x.im;
+                y.re = re + x.re;
+                return y;
+            }
+            // Пример произведения двух комплексных чисел
+            Complex Multi(Complex x)
+            {
+                Complex y;
+                y.im = im * x.im + re * x.im;
+                y.re = re * x.im - im * x.re;
+                return y;
+            }
+            public Complex Minus(Complex x)
+            {
+                Complex y;
+                y.im = im - x.im;
+                y.re = re - x.re;
+                return y;
+            }
+            public string ToString()
+            {
+                return re + "+" + im + "i";
+            }
         }
     }
 }
