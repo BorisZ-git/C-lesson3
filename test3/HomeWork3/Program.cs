1f98767f061059a9a2a7c5c40f85e4f8f344100a
BorisZ-git <ribos@bk.ru>
2017-03-06 14:33:08 +0300

    done with Home Work 3 fuf

--- test3/HomeWork3/Program.cs
+++ test3/HomeWork3/Program.cs
@@ -10,11 +10,11 @@ using System.Threading.Tasks;
  *  Достаточно решить 2 задачи. Все программы сделайть в одном решении. 
  * */
 /* ВОПРОСЫ:
- * Если & - означает И. А | - означает ИЛИ. То почему в данном примере работает | ?
+ * 1.Если & - означает И. А | - означает ИЛИ. То почему в данном примере работает | ?
  * for (int i = 1; i % den != 0 | i % x.den != 0; EqualDen = ++i); (метод Plus)
  * Я так понимаю это из-за результата логической операции, что он возвращает False
  * как только одно из условий перестает выполняться. Но в голове это не укладывается.
- * Ведь логически должно быть: выполняй пока это условие истинно И пока это условие истинно.
+ * Ведь логически должно быть: выполняй пока это условие истинно И пока это условие истинно. 
 */
 
 namespace HomeWork3
@@ -23,56 +23,65 @@ namespace HomeWork3
     {
         static void Main(string[] args)
         {
+            #region Дроби
+            int a1;
+            int b1;
+            int a2;
+            int b2;
+            #endregion            
             //Hello
             Console.WriteLine("Привет! Данная программа поможет" +
-                "осуществить вычисления между дробями");
+"осуществить вычисления между дробями");
             Console.Write("Введите числитель первой дроби: ");
-            int a1 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out a1);
             Console.Write("Введите знаменатель первой дроби: ");
-            int b1 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out b1);
             Console.Write("Введите числитель второй дроби: ");
-            int a2 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out a2);
             Console.Write("Введите знаменатель второй дроби: ");
-            int b2 = int.Parse(Console.ReadLine());
-
-
+            int.TryParse(Console.ReadLine(), out b2);
             Fractions frs1 = new Fractions(a1, b1);
             Fractions frs2 = new Fractions(a2, b2);
-            Fractions frs3 = new Fractions();            
-            frs3 = frs1.Plus(frs2);
-            frs3.ShowRes();
-            //REALIZE NEGATIVE VALUE
-            //REALIZE METHOD SIMPLYFYING
-            frs3 = frs2.Minus(frs1);
-            frs3.ShowRes();
-            frs3 = frs2.Multiplication(frs1);
-            frs3.ShowRes();
-            frs3 = frs2.Division(frs1);
-            frs3.ShowRes();
+            Fractions frs3 = new Fractions();
+
+                frs3 = frs1.Plus(frs2);
+                frs3.ShowRes();
+                frs3 = frs2.Minus(frs1);
+                frs3.ShowRes();
+                frs3 = frs2.Multiplication(frs1);
+                frs3.ShowRes();
+                frs3 = frs2.Division(frs1);
+                frs3.ShowRes();
+
 
             Console.ReadLine();
 
         }
         class Fractions
         {
+            #region Переменные
+            //знак
+            char minus = ' ';
             // целое число
             int integer;
             // числитель
             int num;
             // знаменатель 
             int den;
-            //constructor empty
+            #endregion
+
+            //constructor empty (Пустой контсруктор)
             public Fractions()
             {
 
             }
-            //constructor take value
+            //constructor take value(Принимающий значения)
             public Fractions(int numerator, int denominator)
             {
                 den = denominator;
                 num = numerator;
             }
-            //Realize methods
+            //Realize methods (Методы вычислений)
             public Fractions Plus(Fractions x)
             {
                 Fractions y = new Fractions();
@@ -95,6 +104,7 @@ namespace HomeWork3
                 }
                 //simplifying fractions
                 SetSimply(y);
+                GetInteger(y);
                 return y;
             }            
             public Fractions Minus(Fractions x)
@@ -105,19 +115,20 @@ namespace HomeWork3
                 {
                     y.den = den;
                     y.num = num - x.num;
-                }
-                //We need to set all fractions to one denominator
+                }                
                 else
                 {
-                    //find equal denominator
-                    int EqualDen = 0;
-                    for (int i = 1; i % den != 0 | i % x.den != 0; EqualDen = ++i) ;
-                    //reduce to a common denominator
-                    y.num = (num * (EqualDen / den)) - (x.num * (EqualDen / x.den));
-                    y.den = EqualDen;
+                    Count(this, x, y);
+
+                }                
+                if (y.num < 0)
+                {
+                    y.num = Math.Abs(y.num);
+                    y.minus = '-';
                 }
                 //simplifying fractions
-                SetSimply(y);
+                SetSimply(y);                
+                GetInteger(y);
                 return y;
             }
             public Fractions Multiplication(Fractions x)
@@ -128,6 +139,7 @@ namespace HomeWork3
                 y.den = den * x.den;
                 //simplifying fractions
                 SetSimply(y);
+                GetInteger(y);
                 return y;
             }
             public Fractions Division(Fractions x)
@@ -138,14 +150,20 @@ namespace HomeWork3
                 y.den = den * SwapDen(x.num,x.den);
                 //simplifying fractions
                 SetSimply(y);
+                GetInteger(y);
                 return y;
             }            
-            //Realize method ShowRes
+            //Realize method ShowRes (Метод выводящий значение на консоль)
             public void ShowRes()
             {
-                Console.WriteLine($"{num}/{den}");
+                if (integer != 0)
+                {
+                    Console.WriteLine($"Целая: {minus}{integer} Дробная: {num}/{den}");
+                }
+                else
+                Console.WriteLine($"Дробь: {minus} {num}/{den}");
             }
-            //Get NOD
+            //Методы для вычеслений            
             static int NOD(int a,int b)
             {
                 while (a != b)
@@ -154,21 +172,40 @@ namespace HomeWork3
                 }
                 return a;
             }            
-            static int SwapNum(int num,int den)
+            static int NOK(int a,int b)
+            {
+                return (a * b) / NOD(a, b);
+            }
+            static int Comm(int a,int b, int nok)
+            {
+                return a = a * nok / b;
+            }
+            static void Count (Fractions a, Fractions x, Fractions y)
+            {
+                int nok = NOK(a.den, x.den);
+                y.den = nok;
+                y.num = Comm(a.num,a.den, nok) - Comm(x.num,x.den, nok);
+            }
+            static int SwapNum(int num, int den)
             {
                 num = num + den;
                 den = num - den;
                 num = num - den;
                 return num;
             }
-            static int SwapDen(int num,int den)
+            static int SwapDen(int num, int den)
             {
                 num = num + den;
                 den = num - den;
                 num = num - den;
                 return den;
             }
-            //Simplifying fractions NEED REALIZE
+            //Получение целого
+            static void GetInteger(Fractions y)
+            {
+                for (int i = 0; y.num > y.den; y.integer = ++i) y.num -= y.den;
+            }
+            //Simplifying fractions(Упрощение дроби)
             static void SetSimply(Fractions y)
             {
                 int nod = NOD(y.den, y.num);
