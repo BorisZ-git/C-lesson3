a5e374a875472c6f83679c7a66c27afae915b575
BorisZ-git <ribos@bk.ru>
2017-03-06 17:49:02 +0300

    correct

--- test3/test3/Program.cs
+++ test3/test3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 /* Boris Z
  * Есть задачка следующая: 
  * 1.1 Чертим на консоли квадрат по центру.
@@ -22,22 +23,38 @@ namespace test3
             int StrHorLength, width, height, x, y;
             width = Console.WindowWidth;
             height = Console.WindowHeight;
-            StrHorLength = Hor.Length;          
-            x = (width - StrHorLength)  / 2;
+            StrHorLength = Hor.Length;
+            x = (width - StrHorLength) / 2;
             y = height / 2;
 
             DrawLine(x, y, Hor);
-            DrawLine(x, y+5, Hor);
+            DrawLine(x, y + 5, Hor);
 
-            DrawVerti(x,y);
-            DrawVerti(x+10, y);
-            
+            DrawVerti(x, y);
+            DrawVerti(x + 10, y);
+
             //1.2
+            Random rnd = new Random();
+            Console.SetCursorPosition(62, 18);
+            do
+            {
+                Console.SetCursorPosition(rnd.Next(30, 60), rnd.Next(5, 20));
+                int CursorX = Console.CursorTop;
+                int CursorY = Console.CursorLeft;
+                Thread.Sleep(1000);
+                //не работает условие где то ошибся
+                if (CursorX >= x && CursorX <= x + 10 && CursorY >= y && CursorY <= y + 5)
+                {
+                    Thread.Sleep(1000);
+                    Console.SetCursorPosition(rnd.Next(0, 45), rnd.Next(0, 35));
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Курсор попал в квадрат");
+                }
+                Thread.Sleep(1000);
+            } while (true);
 
 
 
-            Console.ReadLine();
-            
         }
         static void DrawLine(int x,int y, string str)
         {
